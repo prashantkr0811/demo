832a5337cb21a68e372ddec09d6fd06720f60f67
prashantkr0811 <prashantkumar2036@gmail.com>
2024-01-10 18:35:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FaBeer } from "react-icons/fa";
+
+function App() {
+  const [count, setCount] = useState(0)
+  {
+    return (
+      <h3>
+        Lets go for a <FaBeer />?
+      </h3>
+    );
+  }
+  return
+}
+
+export default App
